d50373bff219168163f7fc903b25bec99429a148
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2019-12-14 17:20:05 +0300

    Updates

---
+++ precache-manifest.023232d317448901fa26e14cb023e509.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2c9fdaf3e1b2123449b66fe9c9c0d1",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "38f70cd82a6f534d5d76",
+    "url": "/aviasales/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "62dc537632dc97a30567",
+    "url": "/aviasales/static/css/main.f4a02369.chunk.css"
+  },
+  {
+    "revision": "38f70cd82a6f534d5d76",
+    "url": "/aviasales/static/js/2.24301021.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/aviasales/static/js/2.24301021.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62dc537632dc97a30567",
+    "url": "/aviasales/static/js/main.19cc10b4.chunk.js"
+  },
+  {
+    "revision": "958d36c8ee4f1c94d8c6",
+    "url": "/aviasales/static/js/runtime-main.da907d53.js"
+  },
+  {
+    "revision": "96990a1b3638b68f706e0a3331be2821",
+    "url": "/aviasales/static/media/Logo.96990a1b.svg"
+  }
+]);
